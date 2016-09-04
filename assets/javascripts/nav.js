91c794b47dd0396eba664bdf0a748405515996e7
Mitchell Jackson <mitch@Mitchells-MBP.home>
2016-09-04 13:24:44 +0100

    Move mobile menu out of flow of container

--- assets/javascripts/nav.js
+++ assets/javascripts/nav.js
@@ -4,18 +4,18 @@ define(['jquery'], function ($) {
         /* Responsive menu toggle */             //run when the DOM is ready
         $(".main-menu__toggle").click(function () {        //use a class, since your ID gets mangled
             $("body").toggleClass("menu-open"); //add the class to the clicked element
-            if ($(".main-menu").is(":visible")) {
-                $(".main-menu").hide("slide", { direction: "right" }, 300);
-                $(".main-menu").addClass("hide");
+            if ($(".mobile-nav").is(":visible")) {
+                $(".mobile-nav").hide("slide", { direction: "right" }, 300);
+                $(".mobile-nav").addClass("hide");
             } else {
-                $(".main-menu").show("slide", { direction: "right" }, 300);
-                $(".main-menu").addClass("show");
-                $(".main-menu").removeClass("hide");
+                $(".mobile-nav").show("slide", { direction: "right" }, 300);
+                $(".mobile-nav").addClass("show");
+                $(".mobile-nav").removeClass("hide");
             }
 
             $(window).on("resize", function () { // When resized to large remove display class
                 if ($(window).width() > 1024) {
-                    $('.main-menu').css('display','');
+                    $('.mobile-nav').css('display','');
                     $("body").removeClass("menu-open");
                 }
             }).resize();
