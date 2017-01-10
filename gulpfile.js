64a6fb628222d4853e9260f0ea1734e69689bff2
Sam Bell <me@sam-bell.com>
2017-01-10 07:04:39 +0200

    Enabled incremental build

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var cp = require('child_process');
  */
  gulp.task('jekyll-build', function (done) {
     browserSync.notify('Building Jekyll');
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    return cp.spawn('jekyll', ['build', '--incremental'], {stdio: 'inherit'})
     .on('close', done);
 });
 
